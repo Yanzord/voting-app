57bd2271d5f1648e139debc04f60faca33d332cd
Yan Pereira <yan.pereira@ilegra.com>
2020-05-26 02:17:25 -0300

    Add FeignException classes to be ignored by hystrix fallback.

--- voting-app-service/src/main/java/com/yanzord/votingappservice/exception/NewAgendaException.java
+++ voting-app-service/src/main/java/com/yanzord/votingappservice/exception/NewAgendaException.java
@@ -1,7 +1,7 @@
 package com.yanzord.votingappservice.exception;
 
 public class NewAgendaException extends Exception {
-    public NewAgendaException() {
-        super("Agenda is new, there is no result.");
+    public NewAgendaException(String message) {
+        super(message);
     }
 }
--- voting-app-service/src/main/java/com/yanzord/votingappservice/service/AppService.java
+++ voting-app-service/src/main/java/com/yanzord/votingappservice/service/AppService.java
@@ -38,8 +38,12 @@ public class AppService {
         return votingAgendaClient.saveAgenda(agenda);
     }
 
-    @HystrixCommand(fallbackMethod = "defaultCreateSession", ignoreExceptions = { CreatedSessionException.class })
+    @HystrixCommand(
+            fallbackMethod = "defaultCreateSession",
+            ignoreExceptions = { CreatedSessionException.class, FeignException.NotFound.class, FeignException.BadRequest.class })
     public Session createSession(Session session) throws CreatedSessionException {
+        votingAgendaClient.getAgendaById(session.getAgendaId());
+
         Session sessionFound;
         try {
             sessionFound = votingSessionClient.getSessionByAgendaId(session.getAgendaId());
@@ -64,7 +68,8 @@ public class AppService {
 
     @HystrixCommand(
             fallbackMethod = "defaultRegisterVote",
-            ignoreExceptions = { VoteException.class, ClosedSessionException.class, InvalidCpfException.class })
+            ignoreExceptions = { VoteException.class, ClosedSessionException.class,
+                    InvalidCpfException.class, FeignException.NotFound.class, FeignException.BadRequest.class })
     public Session registerVote(Vote vote, String agendaId) throws VoteException, ClosedSessionException, InvalidCpfException {
         Session session = votingSessionClient.getSessionByAgendaId(agendaId);
 
@@ -95,7 +100,7 @@ public class AppService {
 
             VoteStatus status = VoteStatus.valueOf(json.get("status").asText());
 
-            if(status.equals(VoteStatus.UNABLE_TO_VOTE)) {
+            if (status.equals(VoteStatus.UNABLE_TO_VOTE)) {
                 logger.debug("CPF unable to vote. " + "Session ID: " + session.getId() +
                         "; Agenda ID: " + session.getAgendaId() + "; associate CPF: " + vote.getAssociateCPF());
 
@@ -106,7 +111,7 @@ public class AppService {
             session.setVotes(votes);
 
             return votingSessionClient.saveSession(session);
-        } catch(FeignException.NotFound e) {
+        } catch (FeignException.NotFound e) {
             logger.debug("Invalid CPF. " + "Session ID: " + session.getId() +
                     "; Agenda ID: " + session.getAgendaId() + "; associate CPF: " + vote.getAssociateCPF());
 
@@ -114,30 +119,32 @@ public class AppService {
         }
     }
 
-    @HystrixCommand(fallbackMethod = "defaultAgendaResult", ignoreExceptions = { NewAgendaException.class})
+    @HystrixCommand(
+            fallbackMethod = "defaultAgendaResult",
+            ignoreExceptions = { NewAgendaException.class, FeignException.NotFound.class, FeignException.BadRequest.class })
     public AgendaResult getAgendaResult(String agendaId) throws NewAgendaException {
         Agenda agenda = votingAgendaClient.getAgendaById(agendaId);
 
-        if(agenda.getAgendaResult() == null) {
-            if(agenda.getStatus().equals(AgendaStatus.NEW)) {
+        if (agenda.getAgendaResult() == null) {
+            if (agenda.getStatus().equals(AgendaStatus.NEW)) {
                 Session session = votingSessionClient.getSessionByAgendaId(agendaId);
 
-                if(session.getStatus().equals(SessionStatus.CLOSED)) {
+                if (session.getStatus().equals(SessionStatus.CLOSED)) {
                     return finishAgenda(agenda, generateAgendaResult(session));
                 }
 
-                if(session.getStatus().equals(SessionStatus.OPENED)) {
+                if (session.getStatus().equals(SessionStatus.OPENED)) {
                     if (LocalDateTime.now().isAfter(session.getEndDate())) {
                         closeSession(session);
                         return finishAgenda(agenda, generateAgendaResult(session));
                     }
 
-                    logger.debug("Session for agenda is still open, requested results will not be shown. Agenda ID: " + agendaId);
-                    throw new NewAgendaException();
+                    logger.debug("Session for agenda is still open, requested results will not be show. Agenda ID: " + agendaId);
+                    throw new NewAgendaException("Agenda is new and session is still opened.");
                 }
 
                 logger.debug("No session found for requested agenda results. Agenda ID: " + agendaId);
-                throw new NewAgendaException();
+                throw new NewAgendaException("No session found for requested agenda results.");
             }
         }
 
